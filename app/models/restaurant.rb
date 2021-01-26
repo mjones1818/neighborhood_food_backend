a026290d511c58c1553b2952d87e9b74264ebc5f
mjones1818 <michael.jones0532@gmail.com>
2021-01-25 19:33:12 -0600

    created users table and relationship

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,4 +2,6 @@ class Restaurant < ApplicationRecord
   has_many :restaurant_cuisines
   has_many :cuisines, through: :restaurant_cuisines
   belongs_to :neighborhood
+  has_many :user_restaurants
+  has_many :users,  through: :user_restaurants
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_26_001307) do
+ActiveRecord::Schema.define(version: 2021_01_26_012309) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,6 +51,20 @@ ActiveRecord::Schema.define(version: 2021_01_26_001307) do
     t.integer "neighborhood_id"
     t.string "thumb"
     t.string "featured_image"
+  end
+
+  create_table "user_restaurants", force: :cascade do |t|
+    t.boolean "liked"
+    t.integer "user_id"
+    t.integer "restaurant_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
