b08a8606acdafe00110af8d114a2b23a93bb1bed
mjones1818 <michael.jones0532@gmail.com>
2022-02-06 09:49:53 -0600

    Changed precompile

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,7 @@ module NeigborhoodFood
           :max_age => 0
       end
     end
+    config.assets.initialize_on_precompile = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
